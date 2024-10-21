c836a1bcbcdb27d5d3899df17e4accd26b1b8f4b
AcidSeven33 <neveseduardo357@gmail.com>
2024-10-21 09:39:23 -0300

    first commit

---
+++ Quadrática.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+func main () {
+    int k,l,m;
+    int  arr [10] [10] [10];
+
+    for (k= 0; k<10;k++){
+        for(1=0;1<10;k++){
+            for (m=0;m<10;m++){
+                arr[k][l][m] = 1;
+                fmt.println("Valor do elemento", k, l, m, "é", arr[k][l][m])
+
+    }
+
+}
+    }
+}
